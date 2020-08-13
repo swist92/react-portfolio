601d866a2f98be104b5dbdd2944f52ded4728fb9
Sarah Wist <sarahwist107@gmail.com>
2020-08-12 17:45:31 -0700

    cleaned up code and attempted to fix footer-not yet functioning

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.css";
 
 function Footer() {
   return (
-    <footer className="footer col-12 p-2 pl-0">
+    <footer className="footer col-12 p-5 pl-0">
       <span>Sarah Wist 2020</span>
       <a href="https://www.linkedin.com/in/sarah-wist-a02430103/">LinkedIn</a>
       <a href="https://github.com/swist92">GitHub</a>
